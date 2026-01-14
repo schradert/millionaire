26fc18077b80b1750a1a2c015c7723767df0bfbd
Tristan Schrader <t0rdos@pm.me>
2026-01-13 23:58:48 -0800

    feat: mypy -> ty
    it's so much better...

--- pulumi/default.nix
+++ pulumi/default.nix
@@ -1,9 +1,21 @@
 {
-  perSystem.canivete.devenv.shells.default = {pkgs, ...}: {
+  perSystem.canivete.devenv.shells.default = {
+    lib,
+    pkgs,
+    ...
+  }: {
     packages = [(pkgs.pulumi.withPackages (ps: [ps.pulumi-python]))];
     enterShell = ''
       export PULUMI_ACCESS_TOKEN="$(cat "$DEVENV_ROOT"/secrets/pulumi_token.txt)"
     '';
+    git-hooks.hooks = {
+      mypy.enable = lib.mkForce false;
+      ty = {
+        enable = true;
+        entry = "bash -c 'nix develop --impure --command \"$DEVENV_STATE/venv/bin/ty\" check'";
+        types = ["python"];
+      };
+    };
     languages.python = {
       enable = true;
       directory = "./pulumi";
