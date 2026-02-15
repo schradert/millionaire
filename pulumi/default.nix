0dbd28ceb93b53a35036bbe0a4b01d6193f904a5
Tristan Schrader <t0rdos@pm.me>
2026-02-15 10:08:20 -0800

    chore: bump canivete

--- pulumi/default.nix
+++ pulumi/default.nix
@@ -1,29 +1,7 @@
 {
-  perSystem.canivete.devenv.shells.default = {
-    lib,
-    pkgs,
-    ...
-  }: {
+  perSystem.canivete.devenv.shells.default = {pkgs, ...}: {
+    treefmt.config.settings.formatter.dprint.options = ["--allow-no-files"];
     packages = [(pkgs.pulumi.withPackages (ps: [ps.pulumi-python]))];
-    enterShell = ''
-      export PULUMI_ACCESS_TOKEN="$(cat "$DEVENV_ROOT"/secrets/pulumi_token.txt)"
-    '';
-    git-hooks.hooks = let
-      # Force to use uv-pinned library rather than from nixpkgs
-      exec = exe: args: "bash -c 'nix develop --impure --command \"$DEVENV_STATE/venv/bin/${exe}\" ${args}'";
-    in {
-      ruff.entry = lib.mkForce (exec "ruff" "check --fix");
-      ruff-format.entry = lib.mkForce (exec "ruff" "format");
-      # TODO why is taplo freezing with this pin?
-      # taplo.entry = lib.mkForce (exec "taplo" "fmt");
-      # Much better type checker
-      mypy.enable = lib.mkForce false;
-      ty = {
-        enable = true;
-        entry = exec "ty" "check";
-        types = ["python"];
-      };
-    };
     languages.python = {
       enable = true;
       directory = "./pulumi";
