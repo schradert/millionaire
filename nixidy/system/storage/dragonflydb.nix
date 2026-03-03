2e084975005802ef82f8339d76241c764d55acd4
Tristan Schrader <t0rdos@pm.me>
2026-03-02 23:17:17 -0800

    feat(nixidy): dragonflydb

---
+++ nixidy/system/storage/dragonflydb.nix
@@ -0,0 +1,29 @@
+{
+  nixidy = {pkgs, ...}: let
+    repo = pkgs.fetchFromGitHub {
+      owner = "dragonflydb";
+      repo = "dragonfly-operator";
+      rev = "v1.4.0";
+      hash = "sha256-QBylTbY+8HcD0Z2VyqtwUYm+MeXCP7IwVtJpCCMswXs=";
+    };
+  in {
+    canivete.crds.dragonflydb = {
+      src = repo;
+      prefix = "manifests";
+      match = "^crd\.yaml$";
+    };
+    applications.dragonflydb = {
+      namespace = "storage";
+      helm.releases.dragonflydb = {
+        chart = pkgs.runCommand "dragonfly-operator" {} "cp -aL ${repo}/charts/dragonfly-operator $out";
+        values = {
+          # FIXME activate with prometheus
+          # serviceMonitor.enabled = true;
+          # FIXME activate with grafana
+          # grafanaDashboard.enabled = true;
+          # grafanaDashboard.grafanaOperator.enabled = true;
+        };
+      };
+    };
+  };
+}
