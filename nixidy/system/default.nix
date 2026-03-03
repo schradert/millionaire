00369d2e73ce2fc969cf7acc826f68db45f061ba
Tristan Schrader <t0rdos@pm.me>
2026-03-02 22:58:54 -0800

    feat(nixidy): cert-manager

--- nixidy/system/default.nix
+++ nixidy/system/default.nix
@@ -3,5 +3,15 @@
     ./cicd/argocd.nix
     ./network/cilium.nix
     ./network/coredns.nix
+    ./security/cert-manager.nix
   ];
+  nixidy.applications.namespaces = {
+    namespace = "kube-system";
+    canivete.bootstrap.enable = true;
+    resources.namespaces = {
+      cicd = {};
+      security = {};
+      storage = {};
+    };
+  };
 }
