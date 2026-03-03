4883aaed464ded683f4354712d47ef6659c8abf8
Tristan Schrader <tristan@mill.com>
2026-03-03 00:18:14 -0800

    feat(nixidy): external-secrets

---
+++ nixidy/system/security/external-secrets/default.nix
@@ -0,0 +1,69 @@
+{
+  imports = [./bitwarden.nix];
+  nixidy = {
+    charts,
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    namespace = "security";
+    name = "external-secrets-kubernetes";
+  in {
+    canivete.crds.external-secrets = {
+      prefix = "config/crds/bases";
+      src = pkgs.fetchFromGitHub {
+        owner = "external-secrets";
+        repo = "external-secrets";
+        rev = "v2.0.1";
+        hash = "sha256-VKsruSQIkSkmU7sAznO5Ex/DF3TsykR+Gd5epd42tlw=";
+      };
+    };
+    applications.external-secrets = {
+      namespace = "security";
+      helm.releases.external-secrets = {
+        chart = charts.external-secrets.external-secrets;
+        values.serviceMonitor.enabled = true;
+      };
+      resources = {
+        clusterSecretStores = lib.flip lib.mapAttrs' config.applications.namespaces.resources.namespaces (ns: _:
+          lib.nameValuePair "kubernetes-${ns}" {
+            spec.provider.kubernetes = {
+              auth.serviceAccount = {inherit name namespace;};
+              remoteNamespace = "default";
+              server.caProvider = {
+                type = "ConfigMap";
+                name = "kube-root-ca.crt";
+                inherit namespace;
+                key = "ca.crt";
+              };
+            };
+          });
+        serviceAccounts.${name} = {};
+        clusterRoles.${name}.rules = [
+          {
+            apiGroups = [""];
+            resources = ["secrets"];
+            verbs = ["get" "list" "watch"];
+          }
+          {
+            apiGroups = ["authorization.k8s.io"];
+            resources = ["selfsubjectrulesreviews"];
+            verbs = ["create"];
+          }
+        ];
+        clusterRoleBindings.${name} = {
+          roleRef = {
+            inherit name;
+            kind = "ClusterRole";
+            apiGroup = "rbac.authorization.k8s.io";
+          };
+          subjects = lib.toList {
+            inherit name namespace;
+            kind = "ServiceAccount";
+          };
+        };
+      };
+    };
+  };
+}
