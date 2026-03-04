9bb55a336f9f60bbb03e276d9dd008b1f715e66f
Tristan Schrader <t0rdos@pm.me>
2026-03-04 01:01:11 -0800

    feat(nixidy): cloudflared + external-dns + gateway api

--- nixidy/default.nix
+++ nixidy/default.nix
@@ -4,11 +4,56 @@
     git-hooks.excludes = ["nixidy/generated"];
     git-hooks.hooks.lychee.toml.exclude = ["^.+\\.svc$"];
   };
-  nixidy = {config, ...}: {
-    nixidy.target = {
-      repository = "https://github.com/schradert/millionaire.git";
-      rootPath = "./nixidy/generated/${config.nixidy.env}";
-      branch = "main";
+  nixidy = {
+    can,
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    pulumi = {
+      vals = resourceType: logicalName: attributeKey: let
+        encodedType = lib.replaceStrings ["/" ":"] ["__" "_"] resourceType;
+        # FIXME why doesn't pkgs.fromYAML exist to parse Pulumi.yaml for project name?
+        params = builtins.concatStringsSep "&" [
+          "organization=pulumbus"
+          "project=millionaire"
+          "stack=${config.nixidy.env}"
+        ];
+      in
+        builtins.concatStringsSep "/" [
+          "ref+pulumistateapi:/"
+          encodedType
+          logicalName
+          "outputs"
+          "${attributeKey}?${params}+"
+        ];
+    };
+  in {
+    options.build.scripts.switch = can.package "command to switch cluster manifests" {internal = true;};
+    config = {
+      _module.args.pulumi = pulumi;
+      nixidy.target = {
+        repository = "https://github.com/schradert/millionaire.git";
+        rootPath = "./nixidy/generated/${config.nixidy.env}";
+        branch = "main";
+      };
+      build.scripts.switch = pkgs.writeShellApplication {
+        # Vals needs to run in the project root to read pulumi state
+        name = "nixidy-switch-${config.nixidy.env}";
+        runtimeInputs = [
+          pkgs.git
+          config.build.scripts.nixidy
+          pkgs.vals
+          pkgs.parallel
+        ];
+        text = ''
+          cd "$(git rev-parse --show-toplevel)"
+          nixidy switch ".#${config.nixidy.env}"
+          find "${config.nixidy.target.rootPath}" -name "*.yaml" -type f | \
+            parallel 'vals eval -f {} > {}.tmp && mv {}.tmp {}'
+        '';
+      };
     };
   };
 }
