4c8ce3950cecd37e3dfa6cacfaf1b79bd8057e27
Tristan Schrader <tristan@mill.com>
2026-03-04 22:27:10 -0800

    feat(pulumi/bitwarden): init + cloudflared secret

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,6 +1,7 @@
 import millionaire
 
 import pulumi_cloudflare as cf
+import pulumi_bitwarden as bw
 
 
 class Millionaire:
@@ -9,11 +10,21 @@ class Millionaire:
         # RPI doesn't support kexec
         # millionaire.NixOS("piper", "piper", "--phases disko,install,reboot")
 
-        cf.ZeroTrustTunnelCloudflared(
-            "main",
-            account_id="73c86a33c82d5c90d0feb68269932302",
-            name="main",
-            config_src="local",
+        account_id = "73c86a33c82d5c90d0feb68269932302"
+        tunnel = cf.ZeroTrustTunnelCloudflared(
+            "main", account_id=account_id, name="main", config_src="local"
+        )
+        tunnel_token = cf.get_zero_trust_tunnel_cloudflared_token_output(
+            account_id=account_id, tunnel_id=tunnel.id
+        )
+        key = "cloudflare/tunnel/token"
+        bw.Secret(
+            key,
+            key=key,
+            value=tunnel_token.token,
+            note="Cloudflared Tunnel token",
+            organization_id="ce96e43f-f2ce-4cd7-a36f-b30e0149eeaf",
+            project_id="baf88382-abda-41b2-8d0f-b30e014c2db9",
         )
 
 
