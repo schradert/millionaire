62003b41a8f8444c37499c20b4396b60421e424f
Tristan Schrader <t0rdos@pm.me>
2026-03-04 22:27:10 -0800

    feat(pulumi/bitwarden): init + cloudflared secret

--- pulumi/default.nix
+++ pulumi/default.nix
@@ -1,4 +1,7 @@
-{
+{config, ...}: let
+  inherit (config.canivete.sops) default;
+in {
+  canivete.pkgs.allowUnfree = ["bws"];
   devenv = {
     config,
     pkgs,
@@ -7,6 +10,7 @@
     enterShell = ''
       export PULUMI_ACCESS_TOKEN="$(cat "${config.devenv.root}/secrets/pulumi_token.txt")"
       export CLOUDFLARE_API_TOKEN="$(cat "${config.devenv.root}/secrets/cloudflare_token.txt")"
+      export BWS_ACCESS_TOKEN="$(cd "${config.devenv.root}" && sops --decrypt --extract '["bitwarden"]' "${default}")"
       pulumi stack select prod
     '';
     languages.python.enable = true;
@@ -14,6 +18,10 @@
     editors.zed.enable = true;
     editors.helix.enable = true;
     treefmt.config.settings.formatter.dprint.options = ["--allow-no-files"];
-    packages = [(pkgs.pulumi.withPackages (ps: [ps.pulumi-python]))];
+    packages = with pkgs; [
+      bws
+      (pkgs.pulumi.withPackages (ps: [ps.pulumi-python]))
+    ];
+    git-hooks.hooks.ruff.excludes = ["pulumi/sdks/**"];
   };
 }
