636feeb7aa6b0c70f9596698562f1d3dbaf1b871
Tristan Schrader <t0rdos@pm.me>
2026-03-11 22:33:54 -0700

    feat(nixos): rest of home server nodes (bonobo + chinchilla + dingo)

--- static/server.nix
+++ static/server.nix
@@ -1,5 +1,6 @@
 {
   flake,
+  lib,
   pkgs,
   ...
 }: {
@@ -7,4 +8,11 @@
   canivete.kubernetes.enable = true;
   environment.systemPackages = [pkgs.kubectl];
   roles.nix-remote-builder.schedulerPublicKeys = [flake.config.canivete.meta.people.my.profiles.personal.sshPubKey];
+  # TODO use 9345 supervisor port upstream for RKE2
+  canivete.kubernetes.yaml.server = lib.mkForce "https://sirver:9345";
+  # TODO should this be a default?
+  # Allows nodes to reach others on the same network by names like `sirver`, etc.
+  services.resolved.settings.Resolve.ResolveUnicastSingleLabel = true;
+  # TODO move over old dotfiles modules
+  # dotfiles.profiles.server.enable = true;
 }
