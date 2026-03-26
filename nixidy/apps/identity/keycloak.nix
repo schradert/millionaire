11e733091560e8a6d8ebb2ea601598e5f1768bc0
Tristan Schrader <t0rdos@pm.me>
2026-03-25 21:48:43 -0700

    feat(keycloak): prometheus integration

--- nixidy/apps/identity/keycloak.nix
+++ nixidy/apps/identity/keycloak.nix
@@ -24,6 +24,8 @@ in {
                 KC_PROXY_HEADERS = "xforwarded";
                 KC_HTTP_ENABLED = "true";
                 KC_HEALTH_ENABLED = "true";
+                KC_METRICS_ENABLED = "true";
+                KC_FEATURES = "hostname:v2";
                 KC_BOOTSTRAP_ADMIN_USERNAME = "admin";
               };
               envFrom = [{secretRef.name = "keycloak";}];
@@ -53,7 +55,19 @@ in {
               };
             };
           };
-          service.keycloak.ports.http.port = 8080;
+          service.keycloak.ports = {
+            http.port = 8080;
+            management.port = 9000;
+          };
+          serviceMonitor.keycloak = {
+            serviceName = "keycloak";
+            endpoints = lib.toList {
+              port = "management";
+              scheme = "http";
+              path = "/metrics";
+              interval = "1m";
+            };
+          };
         };
       };
       resources = {
